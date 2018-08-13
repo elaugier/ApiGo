059e9b71a8a159b8c00aff8ae7be7ea3b4ed57a7
Eric Laugier <ela@egen.guru>
2018-08-13 21:21:29 +0200

    add logger

--- cmd/apigo-engine/main.go
+++ cmd/apigo-engine/main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/elaugier/ApiGo/pkg/apigolib"
 )
 
 func main() {
+
+	// args := os.Args
+
+	f, err := os.OpenFile("apigo.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
 	jsonFile, err := os.Open("config/default.json")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println("Successfully Opened config/default.json")
+	log.Println("Successfully Opened config/default.json")
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var config apigolib.ConfigFileEngine
