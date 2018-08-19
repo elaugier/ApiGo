ffe4e436cd07d1403a7d58c73c5e43bf5c210877
Eric Laugier <ela@egen.guru>
2018-08-19 16:10:34 +0200

    update worker processor

--- pkg/apigohelpers/apigohelpers.go
+++ pkg/apigohelpers/apigohelpers.go
@@ -2,20 +2,30 @@ package apigohelpers
 
 //PowershellRun ...
 func PowershellRun(PSModule string, PSCmdLet string, args map[string]string) (int, string, error) {
+
 	return 0, "OK", nil
 }
 
 //PythonRun ...
 func PythonRun(PyEnv string, PyScript string, args map[string]string) (int, string, error) {
+
 	return 0, "OK", nil
 }
 
 //PerlRun ...
 func PerlRun(PerlScript string, args map[string]string) (int, string, error) {
+
+	return 0, "OK", nil
+}
+
+//RubyRun ...
+func RubyRun(RubyScript string, args map[string]string) (int, string, error) {
+
 	return 0, "OK", nil
 }
 
 //CLIRun ...
 func CLIRun(Command string, args map[string]string) (int, string, error) {
+
 	return 0, "OK", nil
 }
--- pkg/apigoprocessor/apigoprocessor.go
+++ pkg/apigoprocessor/apigoprocessor.go
@@ -3,7 +3,11 @@ package apigoprocessor
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+
+	"github.com/elaugier/ApiGo/pkg/apigohelpers"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/elaugier/ApiGo/pkg/apigolib"
@@ -12,25 +16,39 @@ import (
 //Process ...
 func Process(message *kafka.Message) {
 	var msg apigolib.JsonCmd
+	exitCode := 0
+	result := ""
+	var err error
 	r := bytes.NewReader(message.Value)
 	jsonParser := json.NewDecoder(r)
 	jsonParser.Decode(&msg)
 	switch msg.Type {
 	case "Powershell":
 		log.Printf("Powershell type detected (%s)\r\n", msg.Uuid)
-		//TODO: write PowershellHelper
+		exitCode, result, err = apigohelpers.PowershellRun(msg.PSModule, msg.Name, msg.Args)
 	case "Python":
 		log.Printf("Python type detected (%s)\r\n", msg.Uuid)
-		//TODO: write PythonHelper
+		exitCode, result, err = apigohelpers.PythonRun(msg.PyEnv, msg.Name, msg.Args)
 	case "Perl":
 		log.Printf("Perl type detected (%s)\r\n", msg.Uuid)
-		//TODO: write PerlHelper
+		exitCode, result, err = apigohelpers.PerlRun(msg.Name, msg.Args)
+	case "Ruby":
+		log.Printf("Ruby type detected (%s)\r\n", msg.Uuid)
+		exitCode, result, err = apigohelpers.RubyRun(msg.Name, msg.Args)
 	case "CommandLine":
 		log.Printf("CLI type detected (%s)\r\n", msg.Uuid)
-		//TODO: write CLIHelper
+		exitCode, result, err = apigohelpers.CLIRun(msg.Name, msg.Args)
 	default:
 		log.Printf("Unknown type detected for job %s\r\n", msg.Uuid)
+		exitCode = 1
+		result = fmt.Sprintf("Unknown type detected for job %s : %s", msg.Uuid, msg.Type)
+		err = errors.New(result)
 	}
 
+	if exitCode != 0 || err != nil {
+
+	} else {
+
+	}
 	//TODO: Update job state in database
 }
