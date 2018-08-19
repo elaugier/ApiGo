162f5d4b54e5825bc29f4879bd3e3f082c95f326
Eric Laugier <ela@egen.guru>
2018-08-19 17:58:21 +0200

    add db structs

--- pkg/apigodb/apigodb.go
+++ pkg/apigodb/apigodb.go
@@ -1,6 +1,9 @@
 package apigodb
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	//drivers databases
 	_ "github.com/jinzhu/gorm/dialects/mssql"
@@ -8,6 +11,59 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
+
+//Job ...
+type Job struct {
+	ID        uuid.UUID `sql:"type:uuid;primary key;default:uuid_generate_v4()"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+	Message   string
+	Result    string
+	Status    string
+}
+
+//TableName ...
+func (Job) TableName() string {
+	return "jobs"
+}
+
+//Request ...
+type Request struct {
+	ID        uuid.UUID `sql:"type:uuid;primary key;default:uuid_generate_v4()"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+	IP        string
+	Method    string
+	URI       string
+	Browser   string
+	Os        string
+	Status    int
+	Username  string
+	Device    string
+	Duration  float64
+}
+
+//TableName ...
+func (Request) TableName() string {
+	return "requests"
+}
+
+//User ...
+type User struct {
+	ID        uuid.UUID `sql:"type:uuid;primary key;default:uuid_generate_v4()"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+	Username  string
+	Apikey    string
+}
+
+//TableName ...
+func (User) TableName() string {
+	return "users"
+}
 
 //InitDb ...
 func InitDb() {
