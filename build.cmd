a519e9cd600a44ddea5fafdb6671f8e309ebde9e
Eric Laugier <ela@egen.guru>
2018-08-23 21:01:09 +0200

    add build task in vscode

--- build.cmd
+++ build.cmd
@@ -1,12 +1,5 @@
-set GOOS=
-set GOARCH=
-go build ./...
-go install ./...
-go build -o dist\apigo-engine\win-apigo-engine.exe cmd\apigo-engine\main.go
-go build -o dist\apigo-worker\win-apigo-worker.exe cmd\apigo-worker\main.go
-go build -o dist\apigo-authentication-service\win-apigo-authentication-service.exe cmd\apigo-authentication-service\main.go
-go build -o dist\apigo-authorization-service\win-apigo-authorization-service.exe cmd\apigo-authorization-service\main.go
-go build -o dist\apigo-helloworld\win-apigo-helloworld.exe cmd\apigo-helloworld\main.go
+@echo off
+@set "GOOS=" && set "GOARCH=" && go build ./... && go install ./... && go build -o dist\apigo-engine\win-apigo-engine.exe cmd\apigo-engine\main.go && go build -o dist\apigo-worker\win-apigo-worker.exe cmd\apigo-worker\main.go && go build -o dist\apigo-authentication-service\win-apigo-authentication-service.exe cmd\apigo-authentication-service\main.go && go build -o dist\apigo-authorization-service\win-apigo-authorization-service.exe cmd\apigo-authorization-service\main.go && go build -o dist\apigo-helloworld\win-apigo-helloworld.exe cmd\apigo-helloworld\main.go
 REM set GOOS=darwin 
 REM set GOARCH=amd64 
 REM go build dist\apigo-engine\mac-apigo-engine cmd\apigo-engine\main.go
