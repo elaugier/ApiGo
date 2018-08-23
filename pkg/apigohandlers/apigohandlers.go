4af862fbd6de512f8d4d895bfb0a88a349da74b8
Eric Laugier <ela@egen.guru>
2018-08-23 20:44:17 +0200

    add type control

--- pkg/apigohandlers/apigohandlers.go
+++ pkg/apigohandlers/apigohandlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/elaugier/ApiGo/pkg/apigolib"
 	"github.com/spf13/viper"
@@ -25,39 +26,121 @@ func SynchronousJob(c *gin.Context) {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
 	currentRoute := Route.Name
-
+	log.Printf("Current Route => %s", currentRoute)
 	for i := 0; i < len(Route.Cmd.Params); i++ {
 
 		p := Route.Cmd.Params[i]
 
 		log.Printf("Expected parameter name: %s", p.Name)
+		mandatory, err := strconv.ParseBool(p.Mandatory)
+		if err == nil {
+			switch p.In {
+			case "uri":
+				value := c.Param(p.Name)
+				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
 
-		switch p.In {
-		case "uri":
-			value := c.Param(p.Name)
-			log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
 
-		case "header":
-			value := c.GetHeader(p.Name)
-			log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
 
-		case "querystring":
-			value := c.Query(p.Name)
-			log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
 
-		case "body":
-			var keyValue map[string]string
-			c.BindJSON(&keyValue)
-			value := keyValue[p.Name]
-			log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+			case "header":
+				value := c.GetHeader(p.Name)
+				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
 
-		default:
-			log.Printf("Unkown 'In' value for param '%s'", p.Name)
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
 
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
+
+			case "querystring":
+				value := c.Query(p.Name)
+				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
+
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
+
+			case "body":
+				var keyValue map[string]string
+				c.BindJSON(&keyValue)
+				value := keyValue[p.Name]
+				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
+
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
+
+			default:
+				log.Printf("Unkown 'In' value for param '%s'", p.Name)
+				c.JSON(500, gin.H{
+					"msg": fmt.Sprintf("Unkown 'In' value for param '%s'", p.Name),
+				})
+				return
+			}
+		} else {
+			log.Printf("Error while parsing Mandatory option for param %s", p.Name)
+			c.JSON(500, gin.H{
+				"msg": fmt.Sprintf("Error while parsing Mandatory option for param %s", p.Name),
+			})
+			return
 		}
 	}
 
-	log.Printf("Current Route => %s", currentRoute)
 	c.JSON(200, gin.H{
 		"msg": fmt.Sprintf("%s", currentRoute),
 	})
@@ -74,7 +157,7 @@ func AsynchronousJob(c *gin.Context) {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
 	currentRoute := Route.Name
-
+	log.Printf("Current Route => %s", currentRoute)
 	for i := 0; i < len(Route.Cmd.Params); i++ {
 
 		p := Route.Cmd.Params[i]
@@ -89,18 +172,65 @@ func AsynchronousJob(c *gin.Context) {
 
 				if value == "" && mandatory {
 					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
 				}
+
 				if value == "" && !mandatory {
 					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
 				}
 
 			case "header":
 				value := c.GetHeader(p.Name)
 				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
 
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
+
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
+
 			case "querystring":
 				value := c.Query(p.Name)
 				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
+
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
+
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
 
 			case "body":
 				var keyValue map[string]string
@@ -108,17 +238,68 @@ func AsynchronousJob(c *gin.Context) {
 				value := keyValue[p.Name]
 				log.Printf("retrieve key '%s' => '%s' from %s", p.Name, value, p.In)
 
+				if value == "" && mandatory {
+					log.Printf("Parameter '%s' is mandatory => raise error and add message for response", p.Name)
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' is mandatory => raise error and add message for response", p.Name),
+					})
+					return
+				}
+
+				if value == "" && !mandatory {
+					log.Printf("Parameter '%s' is not mandatory but empty => no action", p.Name)
+				}
+
+				if !IsValueTypeOfExpected(value, p.Type) {
+					c.JSON(400, gin.H{
+						"msg": fmt.Sprintf("Parameter '%s' : bad type", p.Name),
+					})
+				}
+
 			default:
 				log.Printf("Unkown 'In' value for param '%s'", p.Name)
-
+				c.JSON(500, gin.H{
+					"msg": fmt.Sprintf("Unkown 'In' value for param '%s'", p.Name),
+				})
+				return
 			}
+		} else {
+			log.Printf("Error while parsing Mandatory option for param %s", p.Name)
+			c.JSON(500, gin.H{
+				"msg": fmt.Sprintf("Error while parsing Mandatory option for param %s", p.Name),
+			})
+			return
 		}
 	}
 
-	log.Printf("Current Route => %s", currentRoute)
 	c.JSON(200, gin.H{
 		"msg": fmt.Sprintf("%s", currentRoute),
 	})
+}
+
+//IsValueTypeOfExpected ...
+func IsValueTypeOfExpected(value string, typeExpected string) bool {
+	switch strings.ToLower(typeExpected) {
+	case "int":
+		_, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return false
+		}
+	case "float":
+		_, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return false
+		}
+	case "uint":
+		_, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return false
+		}
+	case "string":
+	default:
+		return true
+	}
+	return true
 }
 
 //Parameter ...
