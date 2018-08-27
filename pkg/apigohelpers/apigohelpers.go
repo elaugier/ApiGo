11437bb201b1f125a94f15ee4ce32b612a7b7e05
Eric Laugier <ela@egen.guru>
2018-08-27 07:03:37 +0200

    continue to code helpers on workers

--- pkg/apigohelpers/apigohelpers.go
+++ pkg/apigohelpers/apigohelpers.go
@@ -1,31 +1,62 @@
 package apigohelpers
 
+import (
+	"os/exec"
+	"strings"
+)
+
 //PowershellRun ...
 func PowershellRun(PSModule string, PSCmdLet string, args map[string]string) (int, string, error) {
+	binaryName := "powershell.exe"
+	powershellArgs := []string{
+		"-NoProfile",
+		"-ExecutionPolicy", "ByPass",
+		"-Command", "$PSDefaultParameterValues['*:Encoding'] = 'utf8';$ErrorActionPreference=\\\"Stop\\\";Import-Module -Name {PSModule} 3>$null;{Cmd};if($?){Exit(0);}else{Exit(0);};"
+	}
+	for i := 0; i < len(powershellArgs); i++{
+		if(!strings.HasPrefix(powershellArgs[i],"-")){
+			powershellArgs[i] = "\"" + powershellArgs[i] + "\""
+		}
+	}
+	folderPath, err := osext.ExecutableFolder()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var argsinline string
+	for key, value := range args {
+		argsinline += " " + key+ " " + value
+	}
+	powershellCommand = PSCmdLet + " " + argsinline
+
+	exec.Command(binaryName,strings.Join(powershellArgs, " "))
 
 	return 0, "OK", nil
 }
 
 //PythonRun ...
 func PythonRun(PyEnv string, PyScript string, args map[string]string) (int, string, error) {
-
+	binaryName := "python.exe"
+	exec.Command(binaryName,PyScript)
 	return 0, "OK", nil
 }
 
 //PerlRun ...
 func PerlRun(PerlScript string, args map[string]string) (int, string, error) {
-
+	binaryName := "perl.exe"
+	exec.Command(binaryName, PerlScript)
 	return 0, "OK", nil
 }
 
 //RubyRun ...
 func RubyRun(RubyScript string, args map[string]string) (int, string, error) {
-
+	binaryName := "ruby.exe"
+	exec.Command(binaryName, RubyScript)
 	return 0, "OK", nil
 }
 
 //CLIRun ...
 func CLIRun(Command string, args map[string]string) (int, string, error) {
-
+	binaryName := "cmd.exe"
+	exec.Command(binaryName,Command)
 	return 0, "OK", nil
 }
